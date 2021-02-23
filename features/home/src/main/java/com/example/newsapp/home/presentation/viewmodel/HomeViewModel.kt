b6dc14d6460c410b245338bd806bc5d18589b897
joaofgz@hotmail.com <Jvcma123>
2021-02-23 14:51:41 -0300

    Home View Model with channel emitter to viewmodel consume one time !

--- features/home/src/main/java/com/example/newsapp/home/presentation/viewmodel/HomeViewModel.kt
+++ features/home/src/main/java/com/example/newsapp/home/presentation/viewmodel/HomeViewModel.kt
@@ -3,6 +3,7 @@ package com.example.newsapp.home.presentation.viewmodel
 import android.view.WindowManager
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
 import com.example.common.base.BaseViewModel
@@ -15,8 +16,11 @@ import com.example.newsapp.home.presentation.event.HomeEvents
 import com.example.newsapp.home.presentation.state.HomeState
 import com.example.newsapp.local.domain.RemoveSavedTokenUseCase
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.Channel.Factory.UNLIMITED
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 
 class HomeViewModel(
@@ -26,15 +30,18 @@ class HomeViewModel(
     private val favoriteNewsUseCase: FavoriteNewsUseCase,
     getNewsUseCase: GetNewsUseCase
 ) : BaseViewModel<HomeEvents, HomeState>() {
+    private val stateChannel = Channel<HomeState>(UNLIMITED)
+
+
     private val TAG = this@HomeViewModel::class.simpleName
-    private val _state = MutableLiveData<HomeState>()
+
     private val reloadNewsJob: Job? = null
 
     override val removeTokenUseCase: (suspend () -> Unit)?
         get() = removeSavedTokenUseCase::execute
 
     override val state: LiveData<HomeState>
-        get() = _state
+        get() = stateChannel.receiveAsFlow().asLiveData()
 
     val newsListFlow = getNewsUseCase.execute(null).catch { e ->
         if (e is WindowManager.BadTokenException)
@@ -54,12 +61,14 @@ class HomeViewModel(
     private fun getHighlightNews() {
         launchAuthenticated {
             val list = getHighlightsNewsUseCase.execute()
-            _state.value = HomeState.FetchedHighLightsNews(list)
+            stateChannel.send(HomeState.FetchedHighLightsNews(list))
         }
     }
 
     private fun reloadNews() {
-        _state.value = HomeState.ReloadNews
+        viewModelScope.launch {
+            stateChannel.send(HomeState.ReloadNews)
+        }
     }
 
     private fun favoriteItem(news: News) {
@@ -70,9 +79,13 @@ class HomeViewModel(
     }
 
     private fun updateFavoriteState(state: FavoriteNewsUseCase.FavoriteState) {
-        _state.value = when (state) {
-            FavoriteNewsUseCase.FavoriteState.Saved -> HomeState.NewsSavedFavorite
-            FavoriteNewsUseCase.FavoriteState.Removed -> HomeState.NewsRemovedFromFavorite
+        viewModelScope.launch {
+            stateChannel.send(
+                when (state) {
+                    FavoriteNewsUseCase.FavoriteState.Saved -> HomeState.NewsSavedFavorite
+                    FavoriteNewsUseCase.FavoriteState.Removed -> HomeState.NewsRemovedFromFavorite
+                }
+            )
         }
     }
 
